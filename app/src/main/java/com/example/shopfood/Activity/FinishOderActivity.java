01ada0cec1f261fce55ce3227f0a846e787266d7
ADMIN <xuanboy0911>
2023-05-21 01:17:13 +0700

    //kkkk

---
+++ app/src/main/java/com/example/shopfood/Activity/FinishOderActivity.java
@@ -0,0 +1,16 @@
+package com.example.shopfood.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shopfood.R;
+
+public class FinishOderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_oder);
+    }
+}
